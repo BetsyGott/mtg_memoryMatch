30f31ce4cbfbe996083985df581dff445e475c73
Betsy Gottesman <eightamps@gmail.com>
2016-06-22 16:30:55 -0700

    force of will resized

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     function pushImages(){
         imageArray.push('<img src="images/card_faces/blue/air_elemental_sm.jpeg"/>');
         imageArray.push('<img src="images/card_faces/blue/brainstorm_sm.jpeg"/>');
-        imageArray.push('<img src="images/kazuma_front.png"/>');
+        imageArray.push('<img src="images/card_faces/blue/force_of_will_sm.jpeg"/>');
         imageArray.push('<img src="images/natsuki_crop.png"/>');
         imageArray.push('<img src="images/love_machine_crop.png"/>');
         imageArray.push('<img src="images/uncle_mansuke.png"/>');
