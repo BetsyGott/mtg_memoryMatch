6ed35c6f74b39f2ab35cadb7c0ef967f1a8d6c55
Betsy Gottesman <eightamps@gmail.com>
2016-06-24 14:51:08 -0700

    added white images

---
+++ js/decks/white_deck.js
@@ -0,0 +1,64 @@
+
+/**
+ * White deck object
+ * @constructor
+ */
+function RedDeck(parent){
+    this.parent = parent;
+    this.deck = [
+        {
+            name: "Akroma, Angel of Fury",
+            smallImage: "images/card_faces/red/akroma_angel_of_fury_sm.jpg",
+            fullImage: "images/card_faces/red/akroma_angel_of_fury_lg.jpg",
+            ability: "akroma ability tbd"
+        },
+        {
+            name: "Blood Moon",
+            smallImage: "images/card_faces/red/blood_moon_sm.jpg",
+            fullImage: "images/card_faces/red/blood_moon_lg.jpg",
+            ability: "Brainstorm ability tbd"
+        },
+        {
+            name: "Goblin King",
+            smallImage: "images/card_faces/red/goblin_king_sm.jpg",
+            fullImage: "images/card_faces/red/goblin_king_lg.jpg",
+            ability: "goblin King ability tbd"
+        },
+        {
+            name: "Incinerate",
+            smallImage: "images/card_faces/red/incinerate_sm.jpg",
+            fullImage: "images/card_faces/red/incinerate_lg.jpg",
+            ability: "incinerate ability tbd"
+        },
+        {
+            name: "Lightning Bolt",
+            smallImage: "images/card_faces/red/lightning_bolt_sm.jpg",
+            fullImage: "images/card_faces/red/lightning_bolt_lg.jpg",
+            ability: "lightning bolt ability tbd"
+        },
+        {
+            name: "Shivan Dragon",
+            smallImage: "images/card_faces/red/shivan_dragon_sm.jpg",
+            fullImage: "images/card_faces/red/shivan_dragon_lg.jpg",
+            ability: "shivan dragon ability tbd"
+        },
+        {
+            name: "Simian Spirit Guide",
+            smallImage: "images/card_faces/red/simian_spirit_guide_sm.jpg",
+            fullImage: "images/card_faces/red/simian_spirit_guide_lg.jpg",
+            ability: "simian spirit guide ability tbd"
+        },
+        {
+            name: "Starstorm",
+            smallImage: "images/card_faces/red/starstorm_sm.jpg",
+            fullImage: "images/card_faces/red/starstorm_lg.jpg",
+            ability: "starstorm ability tbd"
+        },
+        {
+            name: "Wheel of Fortune",
+            smallImage: "images/card_faces/red/wheel_of_fortune_sm.jpg",
+            fullImage: "images/card_faces/red/wheel_of_fortune_lg.jpg",
+            ability: "wheel of fortune ability tbd"
+        }
+    ];
+}
