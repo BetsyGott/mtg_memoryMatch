cedb261a50837a99a04397c6e256050fa43133a0
Betsy Gottesman <eightamps@gmail.com>
2016-06-28 17:25:54 -0700

    added some more multiplayer init stuff

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@ $(document).ready(function(){
 
     var newMultiplayerGame = new Multiplayer();
 
+    $("#p1-game-area").hide();
+    $("#p2-game-area").hide();
+
     $("#abilityContainer").hide();
 
     $(".mana-symbol").on("click", function(){
 
-        $(".overlay").css("opacity",0.8);
-        $(".overlay").hide();
+        // $(".overlay").css("opacity",0.8);
+        // $(".overlay").hide();
         
         var playerName = $("#playerName").val();
 
         var deckChoice = $(this).attr("data-deck");
 
-        $(".deck-choice").hide();
+        //$(".deck-choice").hide();
         
         var capName = deckChoice[0].toUpperCase() + deckChoice.substring(1) + 'Deck';
         
--- js/multiplayer_controller.js
+++ js/multiplayer_controller.js
@@ -11,9 +11,11 @@ function Multiplayer(){
 
 Multiplayer.prototype.choosePlayers = function(name, deckChoice){
     
-        
+        // console.log("name in chooseplayers, ", name, "deckChoice in chooseplayers: ", deckChoice);
     
         if(this.player1 === null){
+
+            console.log("first player");
             
             var gameArea = $("#p1-game-area");
             //if no player 1 the first player is player1
@@ -25,31 +27,46 @@ Multiplayer.prototype.choosePlayers = function(name, deckChoice){
             this.player1.assignDeck(deckChoice);
 
             //placeholder? customize the ability div to the deck chosen
-            this.createAbilityContainer(gameArea, deckChoice.color, deckChoice.glowColor);
+            this.createAbilityContainer(gameArea, this.player1.deck.color, this.player1.deck.glowColor);
             
             //Assign game to player 1
             this.player1.createNewGame(gameArea);
 
+            this.clearFields();
+
         } else {
+            console.log("second player");
+
             gameArea = $("#p2-game-area");
             
             this.player2 = new Player(name);
 
             //assign deckChoice to player 2 
-            this.player2.assignDeck();
+            this.player2.assignDeck(deckChoice);
 
             //placeholder? customize the ability div to the deck chosen
-            this.createAbilityContainer(gameArea, deckChoice.color, deckChoice.glowColor);
+            this.createAbilityContainer(gameArea, this.player2.deck.color, this.player2.deck.glowColor);
             
             //Assign game to player 2
             this.player2.createNewGame(gameArea);
+
+            this.clearFields();
         }
 
         if(this.player1 && this.player2) {
+            console.log("both players selected");
             //if both players are already created, do a random 50/50 calc to determine who goes first
             var firstPlayer = this.determineFirstPlayer() === 0 ? this.player1 : this.player2;
 
+            console.log(firstPlayer);
+            
+            //hide the intro screen
+            $(".overlay").css("opacity",0.8);
+            $(".overlay").hide();
+            $(".deck-choice").hide();
+            
             //show the game area of the firstPlayer
+            firstPlayer.game.gameArea.show();
         }
 };
 
@@ -59,9 +76,15 @@ Multiplayer.prototype.determineFirstPlayer = function(){
 
 //currently in multiplayer but maybe this should be in a view controller??
 Multiplayer.prototype.createAbilityContainer = function(gameArea, bgColor, glowColor){
+    // console.log("gamearea: ", gameArea, "bgColor: ", bgColor, "glowColor: ", glowColor);
     $("#abilityContainer").css({
         background: 'url("images/blanks/'+bgColor+'_blank.png")no-repeat center center',
         backgroundSize: 'cover',
         boxShadow: '0 0 41px 6px '+glowColor
     });
+};
+
+Multiplayer.prototype.clearFields = function(){
+    $("#playerName").val("");
+
 };
